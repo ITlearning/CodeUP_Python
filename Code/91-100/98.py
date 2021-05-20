0066e4a25764e8097b08137691e4ad75ece2d519
ITlearning <yo7504@naver.com>
2021-05-20 14:39:04 +0900

    05월 20일 코드업 기초 100제 문제 풀이 완료

--- Code/91-100/98.py
+++ Code/91-100/98.py
@@ -3,26 +3,31 @@ d = [[0 for j in range(10)] for i in range(10)]
 for i in range(10) :
     a = [int(x) for x in input().split()]
     for j in range(10) :
-        d[i][j] = a[j-1]
+        d[i][j] = a[j]
 
-X = 2
-Y = 2
-
-while X <= 10 or Y <= 10 or X > 0 or Y > 0 :
-    if(d[X][Y+1] == 2) :
-        d[X][Y+1] = 9
-        break
-    elif(d[X+1][Y] == 2) :
-        d[X+1][Y] = 9
-        break
-
-    if(d[X][Y+1] == 1) :
-        if(d[X+1][Y] != 1) :
-            X += 1
-            d[X][Y] = 9
-    else :
+X = 0
+Y = 1
+while True :
+    if d[X][Y+1] == 0 :
         Y += 1
         d[X][Y] = 9
+    elif d[X][Y+1] == 1 :
+        if d[X+1][Y] == 0 :
+            X += 1
+            d[X][Y] = 9
+        elif d[X+1][Y] == 1:
+            break
+        else :
+            X += 1
+            d[X][Y] = 9
+            break
+    elif d[X][Y+1] == 2 :
+        Y += 1
+        d[X][Y] = 9
+        break
+
+    if X == 9 or Y == 9 :
+        break
 
 for i in range(10) :
     for j in range(10) :
